369fdc18b604c7fa01d902346c2c6ce95da27b10
hsrsbd <hsraffety@gmail.com>
2019-07-09 20:30:36 -0700

    add comment, add post, working on deleting a post.

--- data/posts-router.js
+++ data/posts-router.js
@@ -45,22 +45,71 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// get comments for a post
+router.get('/:id/comments', (req, res) => {
+  console.log(`hit ${req.params.id}/comments`);
+  const { id } = req.params;
+  Posts.findPostComments(id)
+    .then(comments => {
+      if (comments && comments.length) {
+        res.status(200).json(comments);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: 'The comments information could not be retrieved.' });
+    });
+});
+
 // add a post
 router.post('/', async (req, res) => {
-  try {
-    const postItem = await Posts.insert(req.body);
-    res.status(201).json(postItem);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      error: 'There was an error while saving the post to the database'
-    });
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide title and contents for the post' });
+  } else {
+    try {
+      const postItem = await Posts.insert(req.body);
+      res.status(201).json(postItem);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database'
+      });
+    }
   }
 });
 
-function isValidContent(content) {
-  const { title, contents } = message;
-  return title && contents;
+// delete a post
+
+// add a comment to a post
+router.post('/:id/comments', (req, res) => {
+  if (!isValidComment(req.body)) {
+    res.status(400).json({
+      errorMessage: 'Please provide title and contents for the post.'
+    });
+  } else {
+    Posts.insertComment(req.body)
+      .then(comment => {
+        res.status(201).json(comment);
+      })
+      .catch(() => {
+        res.status(500).json({
+          error: 'There was an error while saving the comment to the database.'
+        });
+      });
+  }
+});
+
+function isValidComment(comment) {
+  const { text, post_id } = comment;
+  return text && post_id;
 }
 
 // export
